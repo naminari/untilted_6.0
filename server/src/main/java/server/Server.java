a8d31c77a1a8c7af9e611c5776a4b99cc35fe239
naminari <ol948903@gmail.com>
2023-07-26 13:40:37 +0300

    lol

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,73 @@
+package server;
+
+import cmd.CmdHandler;
+
+import exceptions.CmdArgsAmountException;
+import exceptions.ExecuteException;
+import exceptions.ValidException;
+import utils.Message;
+import utils.Transit;
+import utils.TypeMessage;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private int port;
+    private final CmdHandler handler;
+
+    public Server(int port, CmdHandler handler) {
+        this.port = port;
+        this.handler = handler;
+    }
+
+    public void run() {
+        while (true) {
+            try (ServerSocket server = new ServerSocket(port)) {/// сокет сервера
+                Socket socket = server.accept();                             /// сокет с клиента
+                var readStream = socket.getInputStream();                    /// чтение входящего инпута
+                Transit<? extends Serializable> transit = readTransit(readStream);  /// создание нового транзита из инпута
+                System.out.println("Транзит читается");
+                Message message = new Message(TypeMessage.OK, handler.executeCmd(transit));  /// создание сообщения о корректной передаче данных на сервер и результате выполнения команды
+                System.out.println("Команда выполняется и формируется сообщение");
+                var writeStream = socket.getOutputStream();
+                sendMessage(message, writeStream);
+                System.out.println("Сообщение отправлено");
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException | CmdArgsAmountException | ExecuteException | ValidException |
+                     InvocationTargetException | IllegalAccessException e) {
+                System.out.println(e.getMessage() + "!");
+            }
+        }
+    }
+
+    private Transit<? extends Serializable> readTransit(InputStream stream) throws IOException, ClassNotFoundException {
+        byte[] data = new byte[stream.available()];
+        while (stream.available() > 0) {
+            int count = stream.read(data);
+        }
+        return deserializeTransit(data);
+    }
+
+    private void sendMessage(Message message, OutputStream stream) throws IOException {
+        stream.write(serializeMessage(message));
+    }
+
+    private Transit<? extends Serializable> deserializeTransit(byte[] data) throws IOException, ClassNotFoundException {
+        try (var stream = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            return (Transit<? extends Serializable>) stream.readObject();
+        }
+    }
+
+    private byte[] serializeMessage(Message message) throws IOException {
+        var byteStream = new ByteArrayOutputStream();
+        try (var objectStream = new ObjectOutputStream(byteStream)) {
+            objectStream.writeObject(message);
+            return byteStream.toByteArray();
+        }
+    }
+
+}
